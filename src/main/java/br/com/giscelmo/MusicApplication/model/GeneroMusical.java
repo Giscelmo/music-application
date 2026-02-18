aff620f4ec590cfefc0b3ceafa42b78303cb8dd1
Giscelmo Costa <giscelmo.costa@gmail.com>
2026-02-18 00:00:29 -0300

    feat: add enums GeneroMusical and TipoArtista

---
+++ src/main/java/br/com/giscelmo/MusicApplication/model/GeneroMusical.java
@@ -0,0 +1,18 @@
+package br.com.giscelmo.MusicApplication.model;
+
+public enum GeneroMusical {
+    BLUES,
+    CLASSICO,
+    ELETRONICO,
+    FORRO,
+    FUNK_CARIOCA,
+    HIP_HOP,
+    JAZZ,
+    MPB,
+    POP,
+    RAP,
+    ROCK,
+    SAMBA,
+    SERTANEJO,
+    SOUL
+}
---
+++ src/main/java/br/com/giscelmo/MusicApplication/model/TipoArtista.java
@@ -0,0 +1,7 @@
+package br.com.giscelmo.MusicApplication.model;
+
+public enum TipoArtista {
+    SOLO,
+    DUPLA,
+    BANDA
+}
